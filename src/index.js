707ceeb7c51f79095363b7f95a7d7d248d81b92a
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-07-29 09:37:36 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  createTaskForm.addEventListener("submit", addNewTask);
 });
+
+
+const addNewTask = event => {
+  //this is to preveent the default submit action 
+  //this stopped me from getting the re-direction error
+  event.preventDefault();
+  //we get the new description element by id
+  //then i created the li element and set its value to the textbox value
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newListItem = document.createElement("li");
+  newListItem.innerText = newTaskDescription.value;
+
+  document.getElementById("tasks").appendChild(newListItem);
+  event.target.reset();
+};
+
